51a7e0c3d482fb0d80e193396cd0c6376f6fc7d6
November Foxtrot <november@fox.trot>
2014-01-02 14:10:33 +0900

    err

--- drache.go
+++ drache.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	book := &scripts.Book{Command: os.Args[2], Environment: os.Args[1], Status: 0}
 	book.ParseLayout()
+
+	if err != nil {
+		panic(err)
+	}
+
 	book.Run()
+
 	os.Exit(book.Status)
 }
